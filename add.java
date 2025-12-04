4599ff6f228f3a812e715fecf308336477e81848
Vidyavarshini G <vvce23cse0032@vvce.ac.in>
2025-12-04 22:58:47 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,6 @@
-public class solution{
-  public static void main(String [] args){
-    System.out.println("hello");
-  }
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
+
